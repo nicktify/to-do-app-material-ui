129f1a36724f34b2606027ff18f0ff55564db16c
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2021-02-07 02:08:31 -0300

    hover buttons, handle escape key, save button

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 export default function App() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [completelist, setCompletelist] = useState([]);
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
   const [filteredlist, setFilteredlist] = useState([]);
 
   useEffect(() => {
     handleFilter();
-  }, [filter, completelist])
+  }, [filter, completelist]);
 
   const handleChange = (e) => {
-    setText(e.target.value)
-  }
-  
+    setText(e.target.value);
+  };
+
   const handleFilter = () => {
-    switch(filter) {
-      case 'complete':
-        setFilteredlist(completelist.filter(todo => todo.checked === true))
+    switch (filter) {
+      case "complete":
+        setFilteredlist(completelist.filter((todo) => todo.checked === true));
         break;
-      case 'incomplete':
-        setFilteredlist(completelist.filter(todo => todo.checked === false))
+      case "incomplete":
+        setFilteredlist(completelist.filter((todo) => todo.checked === false));
         break;
       default:
         setFilteredlist(completelist);
     }
-  }
+  };
 
   return (
     <div>
-      <Form 
+      <Form
         filter={filter}
         setFilter={setFilter}
         text={text}
@@ -48,5 +48,5 @@ export default function App() {
         setText={setText}
       />
     </div>
-  )
+  );
 }
